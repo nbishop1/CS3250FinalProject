697f45afb190a639d6500719224d1e01db238c80
Natalee <nataleebishop@archive.weber.edu>
2025-10-03 19:56:25 -0600

    Created Classes: Family, FamilyMember, Merchant

---
+++ FinalProject/src/Family.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Family {
+    private ArrayList<FamilyMember> members;
+    
+    public static Family createDefaultFamily(String playerName) {
+        Family family = new Family();
+        family.addMember(new Player(playerName)); // Player: 1 food/4 days, 1 water/3 days, 1 medicine/7 days
+        family.addMember(new FamilyMember("Mary", 3, 2, 5)); // Wife
+        family.addMember(new FamilyMember("Uncle", 3, 2, 5)); // Uncle
+        family.addMember(new FamilyMember("Luke", 1, 1, 3)); // Son
+        family.addMember(new FamilyMember("Jessie", 1, 1, 3)); // Daughter
+        return family;
+    }
+
+    public Family() {
+        members = new ArrayList<>();
+    }
+
+    public void addMember(FamilyMember member) {
+        members.add(member);
+    }
+
+    public ArrayList<FamilyMember> getMembers() {
+        return members;
+    }
+
+    public FamilyMember getMemberByName(String name) {
+        for (FamilyMember m : members) {
+            if (m.getName().equalsIgnoreCase(name)) return m;
+        }
+        return null;
+    }
+
+    public ArrayList<FamilyMember> getAliveMembers() {
+        ArrayList<FamilyMember> alive = new ArrayList<>();
+        for (FamilyMember m : members) {
+            if (m.isAlive()) alive.add(m);
+        }
+        return alive;
+    }
+
+    public ArrayList<FamilyMember> getDeceasedMembers() {
+        ArrayList<FamilyMember> deceased = new ArrayList<>();
+        for (FamilyMember m : members) {
+            if (!m.isAlive()) deceased.add(m);
+        }
+        return deceased;
+    }
+
+    public void nextDay() {
+        for (FamilyMember m : members) {
+            if (m.isAlive()) m.nextDay();
+        }
+    }
+}
---
+++ FinalProject/src/FamilyMember.java
@@ -0,0 +1,56 @@
+public class FamilyMember {
+    private String name;
+    private int health;
+    private int daysSinceLastFood;
+    private int daysSinceLastWater;
+    private boolean isSickOrInjured;
+    private int daysSinceSickOrInjured;
+    private int foodInterval;
+    private int waterInterval;
+    private int medicineInterval;
+    private boolean alive;
+
+    public FamilyMember(String name, int foodInterval, int waterInterval, int medicineInterval) {
+        this.name = name;
+        this.health = 100;
+        this.daysSinceLastFood = 0;
+        this.daysSinceLastWater = 0;
+        this.isSickOrInjured = false;
+        this.daysSinceSickOrInjured = 0;
+        this.foodInterval = foodInterval;
+        this.waterInterval = waterInterval;
+        this.medicineInterval = medicineInterval;
+        this.alive = true;
+    }
+
+    public String getName() { return name; }
+    public int getHealth() { return health; }
+    public void setHealth(int health) { this.health = health; }
+    public boolean isAlive() { return alive; }
+    public void setAlive(boolean alive) { this.alive = alive; }
+    public boolean isSickOrInjured() { return isSickOrInjured; }
+    public void setSickOrInjured(boolean sickOrInjured) { this.isSickOrInjured = sickOrInjured; }
+    public int getDaysSinceSickOrInjured() { return daysSinceSickOrInjured; }
+    public void setDaysSinceSickOrInjured(int days) { this.daysSinceSickOrInjured = days; }
+    public int getFoodInterval() { return foodInterval; }
+    public int getWaterInterval() { return waterInterval; }
+    public int getMedicineInterval() { return medicineInterval; }
+    public int getDaysSinceLastFood() { return daysSinceLastFood; }
+    public void setDaysSinceLastFood(int days) { this.daysSinceLastFood = days; }
+    public int getDaysSinceLastWater() { return daysSinceLastWater; }
+    public void setDaysSinceLastWater(int days) { this.daysSinceLastWater = days; }
+
+    public void nextDay() {
+        daysSinceLastFood++;
+        daysSinceLastWater++;
+        if (isSickOrInjured) daysSinceSickOrInjured++;
+    }
+
+    public boolean needsFood() { return daysSinceLastFood >= foodInterval; }
+    public boolean needsWater() { return daysSinceLastWater >= waterInterval; }
+    public boolean needsMedicine() { return isSickOrInjured && daysSinceSickOrInjured >= medicineInterval; }
+
+    public void feed() { daysSinceLastFood = 0; }
+    public void giveWater() { daysSinceLastWater = 0; }
+    public void heal() { isSickOrInjured = false; daysSinceSickOrInjured = 0; }
+}
